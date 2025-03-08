39bc9cac59b30dfd50cdda35290913860b156c3b
Omar-36910 <omarbarros@gmail.com>
2025-03-08 03:16:23 -0300

    Iniciando el 2do  sprint

--- app.js
+++ app.js
@@ -38,6 +38,23 @@ async function insertSuperHero() {
     console.log('Superheroe insertado:', hero);   
 }
 
+async function insertSuperHero() {
+    const hero = new SuperHero({
+        nombreSuperHeroe: 'Ironman',
+        nombreReal: 'Tony Stark',
+        edad: 45,
+        planetaOrigen: 'Tierra',
+        debilidad: 'Depende de la tacnologia',
+        poderes: ['Armadura blindada', 'Volar', 'Laseres'],
+        aliados: ['Spiderman'],
+        enemigos: ['Mandarin'],
+        creador: 'Omar'
+    });
+    await hero.save();
+    console.log('Superheroe insertado:', hero);   
+}
+
+
 insertSuperHero();
 
 
